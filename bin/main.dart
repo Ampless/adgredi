912bcc458d8ee60f304c6a0e55e73a98cc20102d
chris <chrissx@chrissx.de>
2021-05-25 16:35:49 +0200

    a lot, it now does what it should

--- bin/main.dart
+++ bin/main.dart
@@ -1,99 +1,104 @@
+import 'dart:convert';
 import 'dart:io';
-
 import 'dart:typed_data';
 
+import 'package:pedantic/pedantic.dart';
 import 'package:utf/utf.dart';
 
-void singleCheck(
+Future<void> singleCheck(
   InternetAddress addr,
   List<int> packet,
   void Function(List<int>) callback,
-) {
-  Socket.connect(addr, 25565).then((sock) {
-    var data = <int>[];
-    sock.listen(
-      (event) => data.addAll(event),
-      onError: (e) => print(e is Error ? '$e\n\n${e.stackTrace}' : e),
-      onDone: () => callback(data),
-    );
-    sock.add(packet);
-  });
-}
+) =>
+    Socket.connect(addr, 25565, timeout: Duration(seconds: 3)).then((sock) {
+      final data = <int>[];
+      sock.listen(
+        (event) => data.addAll(event),
+        onError: (e) => print(e is Error ? '$e\n\n${e.stackTrace}' : e),
+        onDone: () => callback(data),
+      );
+      sock.add(packet);
+    });
 
-List<int> oldPacket(InternetAddress addr) {
-  var l = [
-    0xFE,
-    0x01,
-    0xFA,
-    0x00,
-    0x0B,
-    0x00,
-    0x4D,
-    0x00,
-    0x43,
-    0x00,
-    0x7C,
-    0x00,
-    0x50,
-    0x00,
-    0x69,
-    0x00,
-    0x6E,
-    0x00,
-    0x67,
-    0x00,
-    0x48,
-    0x00,
-    0x6F,
-    0x00,
-    0x73,
-    0x00,
-    0x74
-  ];
-  l.add(7 + 2 * addr.host.length);
-  l.add(0x4a);
-  l.addAll(encodeUtf16be(addr.host));
-  l.addAll([0x00, 0x00, 0x63, 0xdd]);
-  return l;
-}
+List<int> oldPacket(InternetAddress addr) => [
+      0xFE,
+      0x01,
+      0xFA,
+      0x00,
+      0x0B,
+      0x00,
+      0x4D,
+      0x00,
+      0x43,
+      0x00,
+      0x7C,
+      0x00,
+      0x50,
+      0x00,
+      0x69,
+      0x00,
+      0x6E,
+      0x00,
+      0x67,
+      0x00,
+      0x48,
+      0x00,
+      0x6F,
+      0x00,
+      0x73,
+      0x00,
+      0x74,
+      7 + 2 * addr.host.length,
+      0x4a,
+      ...encodeUtf16be(addr.host),
+      0x00,
+      0x00,
+      0x63,
+      0xdd
+    ];
 
-List<int> newPacket(InternetAddress addr) {
-  var l = [0x00, 0x00];
-  final host = encodeUtf8(addr.host);
-  l.add(host.length);
-  l.addAll(host);
-  l.addAll([0x63, 0xdd, 0x01]);
-  return l;
-}
+// TODO: https://wiki.vg/Server_List_Ping#Current
+//List<int> newPacket(InternetAddress addr) {
+//  final host = utf8.encode(addr.host);
+//  return [0x00, 0x00, host.length, ...host, 0x63, 0xdd, 0x01];
+//}
 
-Map<int, List<int>> oldChecks = {};
-Map<int, List<int>> newChecks = {};
+Map<List<int>, List<int>> checks = {};
+int blocker = 0;
 
-void check(List<int> ip) {
+Future<void> check(List<int> ip) async {
+  blocker++;
   final addr = InternetAddress.fromRawAddress(Uint8List.fromList(ip));
-  singleCheck(addr, oldPacket(addr), (oldCheck) {
-    oldChecks[1] = oldCheck;
-    Future.delayed(
-      Duration(seconds: 1),
-      () => singleCheck(addr, newPacket(addr), (newCheck) {
-        newChecks[1] = newCheck;
-      }),
-    );
-  });
-}
-
-void checkThing() {
-  Future.delayed(Duration(milliseconds: 100), () {
-    if (newChecks.isEmpty)
-      checkThing();
-    else {
-      print(decodeUtf8(oldChecks[1]));
-      print(decodeUtf8(newChecks[1]));
-    }
-  });
+  try {
+    await singleCheck(addr, oldPacket(addr), (oc) {
+      checks[ip] = oc;
+      print('Got response from $ip.');
+      blocker--;
+    });
+  } catch (e) {
+    blocker--;
+  }
 }
 
 void main(List<String> arguments) async {
-  check([212, 224, 77, 85]); //this is just gommehd.net, it works.
-  checkThing();
+  for (var b1 = 10; b1 < 200; b1++) {
+    await File('servers.json').writeAsString(jsonEncode(checks));
+    for (var b2 = 1; b2 < 256; b2++) {
+      for (var b3 = 1; b3 < 256; b3++) {
+        print('$b1.$b2.$b3.%');
+        for (var b4 = 1; b4 < 256; b4++) {
+          unawaited(check([b1, b2, b3, b4]));
+          await Future.delayed(
+              Duration(milliseconds: 1),
+              () async => blocker > 500
+                  ? await Future.delayed(Duration(seconds: 1))
+                  : null);
+        }
+      }
+    }
+  }
+
+  while (blocker > 0) await Future.delayed(Duration(milliseconds: 100));
+
+  await File('servers.json').writeAsString(jsonEncode(checks));
 }
